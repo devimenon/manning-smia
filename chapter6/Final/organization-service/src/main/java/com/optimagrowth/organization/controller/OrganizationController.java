490844537987516875ca13ff1a83444b04f6d96d
dmenon <devi.menon@gmail.com>
2022-09-22 23:03:57 +0530

    - keycloak added to compose; point localhost to keycloak in hosts file; spring security role based access to methods in organization service

--- chapter6/Final/organization-service/src/main/java/com/optimagrowth/organization/controller/OrganizationController.java
+++ chapter6/Final/organization-service/src/main/java/com/optimagrowth/organization/controller/OrganizationController.java
@@ -1,7 +1,10 @@
 package com.optimagrowth.organization.controller;
 
+import javax.annotation.security.RolesAllowed;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,11 +19,20 @@ public class OrganizationController {
     @Autowired
     private OrganizationService service;
 
-
+    @RolesAllowed({ "ADMIN", "USER" }) 
     @RequestMapping(value="/{organizationId}",method = RequestMethod.GET)
     public ResponseEntity<Organization> getOrganization( @PathVariable("organizationId") String organizationId) {
     	Organization org = new Organization(); org.setContactName("somename");
         return ResponseEntity.ok(org);
     }
+    
+    @RolesAllowed("ADMIN")    
+    @RequestMapping(value="/admin/{organizationId}",method = RequestMethod.GET)
+   // @ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Organization> deleteLicense(@PathVariable("organizationId") String organizationId) {
+		//
+    	Organization org = new Organization(); org.setContactName("deleted somename");
+        return ResponseEntity.ok(org);
+	}
 
 }
